fb9fca7ea2a0654a17dcec5d7c087249f016209b
José Dias <fjddias@outlook.com>
2025-01-02 23:09:09 -0300

    Created App urls

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("recipes.urls"))
+    
 ]

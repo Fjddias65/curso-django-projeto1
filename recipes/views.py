ebb4e9808cf7ddac1f3689d12a603854d58de310
José Dias <fjddias@outlook.com>
2025-01-09 22:14:58 -0300

    Update recipes

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,3 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
